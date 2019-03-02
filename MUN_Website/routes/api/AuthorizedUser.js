fd84de940777b0dcfd856b6971b2ba3bde056b03
AmrElseddik <amrmostafa18998@gmail.com>
2019-03-02 22:02:28 +0200

    Done with my task

--- MUN_Website/routes/api/AuthorizedUser.js
+++ MUN_Website/routes/api/AuthorizedUser.js
@@ -8,29 +8,87 @@ const AuthorizedUserArr = [
     new AuthorizedUser('7ooda')
 ];
 
-//
-router.delete('/DeleteAccount/:id',(req,res) => 
+// update an acoount 
+router.put('/UpdateAccount/:id',(req,res) => 
 {
-    const isEntered = EventsArr.some(Event => Event.Account_id===parseInt(req.params.id));
+    const isEntered = AccountsArr.some(Account => Account.Account_id===parseInt(req.params.id));
     if(isEntered)
     {
-        AccountsArr.forEach(Event => 
+        //update name
+        const Account=req.name;
+        AccountArr.forEach(Account => 
         {
-            if (Accounts.Account_id===parseInt(req.params.id))
+            if (Account.name===req.param.name)
+            {
+                Account.name=AccountUpdated.name?AccountUpdated.name:Account.name;
+                Account.details=AccountUpdated.details?AccountUpdated.details:Account.details;
+                res.json({msg: 'The Account is updated successfully', Account});
+            }
+            else
+            {
+              res.status(404).json({msg: 'Nothing have changed'})  
+            }
+        })
+        //update password
+        if( Account.password===req.param.password){
+
+            Account.password=AccountUpdated.password?AccountUpdated.password:Account.password;
+            Account.details=AccountUpdated.details?AccountUpdated.details:Account.details;
+            res.json({msg: 'The Account is updated successfully', Account});
+        
+        }
+        else
+        {
+          res.status(404).json({msg: 'Nothing have changed'})  
+        }
+        // update age 
+        if (Account.age===req.param.age)
+            {
+                Account.age=AccountUpdated.age?AccountUpdated.age:Account.age;
+                Account.details=AccountUpdated.details?AccountUpdated.details:Account.details;
+                res.json({msg: 'The Account is updated successfully', Account});
+            }
+            else
+            {
+              res.status(404).json({msg: 'Nothing have changed'})  
+            }
+        // update major
+        if (Account.major===req.param.major)
+            {
+                Account.major=AccountUpdated.major?AccountUpdated.major:Account.major;
+                Account.details=AccountUpdated.details?AccountUpdated.details:Account.details;
+                res.json({msg: 'The Account is updated successfully', Account});
+            }
+            else
+            {
+              res.status(404).json({msg: 'Nothing have changed'})  
+            }
+    }
+    // to delete my account (to delete an account as an admin will be implemented in the front end)
+    router.delete('/DeleteAccount/:id',(req,res) => 
+{
+    const isEntered = AccountsArr.some(Account => Account.Account_id===parseInt(req.params.id));
+    if(isEntered)
+    {
+        AccountsArr.forEach(Account => 
+        {
+            if (Account.Account_id===parseInt(req.params.id))
             { 
-                delete Accounts.Account_id;
-                delete Accounts.username;
-                delete Accounts.password;
-                delete Accounts.age;
-                delete Accounts.major;
-                res.json({msg:'The account is deleted successfully', AccountsArr});
+                delete Account.Account_id;
+                delete Account.name;
+                delete Account.password;
+                delete Account.age;
+                delete Account.major;
+                delete Account.id;
+                res.json({msg:'The Account is deleted successfully', AccountsArr});
             }
         })
     }
     else
     {
-      res.status(404).json({msg: 'Error on deleting the account'})  
+      res.status(404).json({msg: 'Error on deleting the Account'})  
     }
+});
 });
 
 module.exports = router
