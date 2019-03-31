8ea892759f2aab3e3c069698de2532292b8addf8
seifsallam10 <seifeldinsallam@gmail.com>
2019-03-31 22:23:06 +0200

    Modified the file structure

--- generate.js
+++ generate.js
@@ -5,62 +5,64 @@ const nfetch = require("node-fetch");
 const PORT = 3000;
 
 // DB Config
-const db = require("./config/Keys_Dev").mongoURI;
+const db = require("./config/keys").mongoURI;
 
 // Connect to mongo
-mongoose
-  .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch(err => console.log(err));
+const connectToMongo = () => mongoose
+    .connect(db, { useNewUrlParser: true })
 
 // Clean database first
-mongoose.connection.dropDatabase();
+const dropMongo = () => mongoose.connection.dropDatabase();
 
-async function genAll() {
-  const createEvent = async requestBody => {
-    return new Promise(async (resolve, reject) => {
-      const response = await nfetch(`http://localhost:${PORT}/api/events/`, {
+const createEvent = requestBody => {
+  return new Promise(async (resolve, reject) => {
+    const response = await nfetch(`http://localhost:${PORT}/api/events/`, {
+      method: "POST",
+      body: JSON.stringify(requestBody),
+      headers: { "Content-Type": "application/json" }
+    });
+    const jsonResponse = await response.json();
+    if ("data" in jsonResponse) {
+      resolve(jsonResponse.data);
+    } else {
+      reject(jsonResponse.error);
+    }
+  });
+};
+
+const createPortalLibrary = async requestBody => {
+  return new Promise(async (resolve, reject) => {
+    const response = await nfetch(
+      `http://localhost:${PORT}/api/portal_libraries/`,
+      {
         method: "POST",
         body: JSON.stringify(requestBody),
         headers: { "Content-Type": "application/json" }
-      });
-      const jsonResponse = await response.json();
-      if ("data" in jsonResponse) {
-        resolve(jsonResponse.data);
-      } else {
-        reject(jsonResponse.error);
       }
-    });
-  };
+    );
+    const jsonResponse = await response.json();
+    if ("data" in jsonResponse) {
+      resolve(jsonResponse.data);
+    } else {
+      reject(jsonResponse.error);
+    }
+  });
+};
 
+const generateAll = async () => {
+
+  connectToMongo()
+    .then(() => console.log("Connected to MongoDB"))
+    .catch(err => console.log(err));
+  await dropMongo()
   await createEvent({
     name: "Sokna",
     details: "from 12-5-2019 to 20-5-2019"
   });
-
-  const createPortalLibrary = async requestBody => {
-    return new Promise(async (resolve, reject) => {
-      const response = await nfetch(
-        `http://localhost:${PORT}/api/portal_libraries/`,
-        {
-          method: "POST",
-          body: JSON.stringify(requestBody),
-          headers: { "Content-Type": "application/json" }
-        }
-      );
-      const jsonResponse = await response.json();
-      if ("data" in jsonResponse) {
-        resolve(jsonResponse.data);
-      } else {
-        reject(jsonResponse.error);
-      }
-    });
-  };
-
   await createPortalLibrary({
     title: "Sokna",
     details: "from 12-5-2019 to 20-5-2019"
   });
 }
 
-genAll();
+generateAll();
