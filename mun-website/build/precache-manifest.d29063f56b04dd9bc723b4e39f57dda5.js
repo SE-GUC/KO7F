8c1a8001d9a9689416e207df228c4cf94eed161e
seifsallam10 <seifeldinsallam@gmail.com>
2019-04-16 00:44:25 +0200

    rate event

---
+++ mun-website/build/precache-manifest.d29063f56b04dd9bc723b4e39f57dda5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ad2daa69e431560e47b",
+    "url": "/static/css/main.3708dc92.chunk.css"
+  },
+  {
+    "revision": "7ad2daa69e431560e47b",
+    "url": "/static/js/main.42dffe92.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "873f98bbcbfb10fff842",
+    "url": "/static/js/2.3fe1effc.chunk.js"
+  },
+  {
+    "revision": "6deb41eb62d091e3967af28f02fe51a9",
+    "url": "/index.html"
+  }
+];
