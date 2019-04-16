3d0bc3b9959273f0a3de17c10ef7fd47cf16330d
amrhasanali <amrsalem1998@gmail.com>
2019-04-16 04:50:21 -1000

    show and update FAQ

--- munwebsite-app/src/App.js
+++ munwebsite-app/src/App.js
@@ -4,6 +4,7 @@ import Login from "./pages/Login";
 import Home from "./pages/Home";
 import WelcomePage from "./pages/WelcomePage";
 import Events from "./pages/Events";
+import FAQS_Update_Page from "./pages/FAQS_Update";
 import RegisterUser from "./pages/RegisterUser";
 
 class App extends Component {
@@ -15,6 +16,7 @@ class App extends Component {
           <div className="container">
             <Route exact path="/login" component={Login} />
             <Route exact path="/home-page" component={Home} />
+            <Route exact path="/faqs/:id" component={FAQS_Update_Page} />
             <Route exact path="/events" component={Events} />
             <Route exact path="/register" component={RegisterUser} />
           </div>
